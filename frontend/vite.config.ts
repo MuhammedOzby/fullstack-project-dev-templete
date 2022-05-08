5cdfb3d36b4b69347bbc8c260ba003833067337e
Muhammed Mustafa Özbay <muhammed.ozby@outlook.com>
2022-05-08 17:35:27 +0300

    add extras.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: parseInt(process.env.PORT),
+    watch: {
+      usePolling: true,
+    },
+  },
   plugins: [react()],
   build: {
     outDir: "../backend/public",
